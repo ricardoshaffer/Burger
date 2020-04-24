be95e033fc8e8ae7f795753a70a26a0a263b8090
Ricardo Shaffer <ricardo.ramiro.shaffer@gmail.com>
2020-04-24 19:30:07 -0400

    updatd connection to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 let connection;
 
-// if(process.env.JAWSDB_URL){
-//     serverLink = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
+ if(process.env.JAWSDB_URL){
+     serverLink = mysql.createConnection(process.env.JAWSDB_URL);
+ } else {
     connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
@@ -11,7 +11,7 @@ let connection;
         password: "YOURLOCALPS",
         database: "burger_list"
     })
-//}
+}
    
   connection.connect();
   module.exports = connection;
